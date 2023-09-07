58539d1ee816fc11d5b6adb41f2fbecf7e341936
Edgardo Fernández <efernandeza@odybank.com.pe>
2023-09-06 20:52:03 -0500

    primer commit

---
+++ 20200100_20200200_20200175_Dice__Roller2/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "20200100_20200200_20200175_Dice_Roller"
+include(":app")
